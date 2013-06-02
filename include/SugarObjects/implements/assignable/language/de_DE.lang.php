677642eadda03f6e09cf3ce730100e8200a0cc33
tiv-source <opensource@tiv-consulting.de>
2013-06-02 15:35:14 +0200

    Fix construct.

--- include/SugarObjects/implements/assignable/language/de_DE.lang.php
+++ include/SugarObjects/implements/assignable/language/de_DE.lang.php
@@ -2,8 +2,8 @@
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 $mod_strings = array(
-	'LBL_ASSIGNED_TO_ID'   => 'Zugewiesene Benutzer Id',
-	'LBL_ASSIGNED_TO_NAME' => 'Unterstellt',
+	'LBL_ASSIGNED_TO_ID'   => 'Zugewiesen an Id',
+	'LBL_ASSIGNED_TO_NAME' => 'Zugewiesen an',
 );
 
 ?>
