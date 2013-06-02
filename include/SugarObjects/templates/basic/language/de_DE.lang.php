148c4281cce5b3efe8f6a9faeed889bff1e76ac3
tiv-source <opensource@tiv-consulting.de>
2013-06-02 14:34:50 +0200

    Folder include/SugarObjects/templates/basic/ complete.

---
+++ include/SugarObjects/templates/basic/language/de_DE.lang.php
@@ -0,0 +1,23 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+$mod_strings = array(
+	'LBL_ID'           =>'ID',
+	'LBL_DATE_ENTERED' =>'Datum erstellt',
+	'LBL_DATE_MODIFIED'=>'Datum bearbeitet',
+	'LBL_MODIFIED'     =>'Bearbeitet von',
+	'LBL_MODIFIED_ID'  =>'Bearbeitet von Id',
+	'LBL_MODIFIED_NAME'=>'Bearbeitet von Name',
+	'LBL_CREATED'      =>'Erstellt von',
+	'LBL_CREATED_ID'   =>'Erstellt von Id',
+	'LBL_DESCRIPTION'  =>'Beschreibung',
+	'LBL_DELETED'      =>'Gelöscht',
+	'LBL_NAME'         =>'Name',
+	'LBL_CREATED_USER' =>'Erstellt von Benutzer',
+	'LBL_MODIFIED_USER'=>'Verändert von Benutzer',
+	'LBL_LIST_NAME'    =>'Name',
+	'LBL_EDIT_BUTTON'  =>'Bearbeiten',
+	'LBL_REMOVE'       =>'Löschen',
+);
+
+?>
