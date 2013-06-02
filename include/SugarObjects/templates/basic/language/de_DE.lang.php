fea7730a88c0dd0f955b4503c6d29d0a9604ef77
tiv-source <opensource@tiv-consulting.de>
2013-06-02 18:22:11 +0200

    Fix file banner.

--- include/SugarObjects/templates/basic/language/de_DE.lang.php
+++ include/SugarObjects/templates/basic/language/de_DE.lang.php
@@ -1,14 +1,14 @@
 <?php
 /*
  * Language - Files for Sugar Community Edition
-* Version 6.5.13
-*
-* Author: Marc Michele
-* Company: T.I.V. Consulting GmbH
-* URL: http://www.tiv-consulting.de/
-*
-*
-*/
+ * Version 6.5.13
+ *
+ * Author: Marc Michele
+ * Company: T.I.V. Consulting GmbH
+ * URL: http://www.tiv-consulting.de/
+ *
+ *
+ */
 
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
