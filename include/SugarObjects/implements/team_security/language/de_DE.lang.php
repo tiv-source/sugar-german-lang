341b7557163c3cd380b5d13ecbd43fd17a7d837f
tiv-source <opensource@tiv-consulting.de>
2013-06-02 17:24:07 +0200

    File banner added.

--- include/SugarObjects/implements/team_security/language/de_DE.lang.php
+++ include/SugarObjects/implements/team_security/language/de_DE.lang.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * Language - Files for Sugar Community Edition
+* Version 6.5.13
+*
+* Author: Marc Michele
+* Company: T.I.V. Consulting GmbH
+* URL: http://www.tiv-consulting.de/
+*
+*
+*/
+
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 $mod_strings= array (
