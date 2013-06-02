379979ecd5d70d83c9a89e772c98439c3290ed6d
tiv-source <opensource@tiv-consulting.de>
2013-06-02 20:06:34 +0200

    Code cleanup.

--- include/SugarObjects/templates/issue/language/application/de_DE.lang.php
+++ include/SugarObjects/templates/issue/language/application/de_DE.lang.php
@@ -15,38 +15,35 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 $object_name = strtolower($object_name);
 $app_list_strings = array (
 
-		$object_name.'_type_dom' =>
-		array (
-				'Administration' => 'Administration',
-				'Product' => 'Product',
-				'User' => 'User',
-		),
-		$object_name.'_status_dom' =>
-		array (
-				'New' => 'New',
-				'Assigned' => 'Assigned',
-				'Closed' => 'Closed',
-				'Pending Input' => 'Pending Input',
-				'Rejected' => 'Rejected',
-				'Duplicate' => 'Duplicate',
-		),
-		$object_name.'_priority_dom' =>
-		array (
-				'P1' => 'High',
-				'P2' => 'Medium',
-				'P3' => 'Low',
-		),
-		$object_name.'_resolution_dom' =>
-		array (
-				'' => '',
-				'Accepted' => 'Accepted',
-				'Duplicate' => 'Duplicate',
-				'Closed' => 'Closed',
-				'Out of Date' => 'Out of Date',
-				'Invalid' => 'Invalid',
-		),
+	$object_name.'_type_dom' =>	array (
+		'Administration' => 'Administration',
+		'Product' => 'Product',
+		'User' => 'User',
+	),
+
+	$object_name.'_status_dom' => array (
+		'New' => 'New',
+		'Assigned' => 'Assigned',
+		'Closed' => 'Closed',
+		'Pending Input' => 'Pending Input',
+		'Rejected' => 'Rejected',
+		'Duplicate' => 'Duplicate',
+	),
+
+	$object_name.'_priority_dom' => array (
+		'P1' => 'High',
+		'P2' => 'Medium',
+		'P3' => 'Low',
+	),
+
+	$object_name.'_resolution_dom' => array (
+		'' => '',
+		'Accepted' => 'Accepted',
+		'Duplicate' => 'Duplicate',
+		'Closed' => 'Closed',
+		'Out of Date' => 'Out of Date',
+		'Invalid' => 'Invalid',
+	),
 );
-
-
 
 ?>
