9b1ad372628ec69aa6fba6ee60b0a6d22606bbfe
tiv-source <opensource@tiv-consulting.de>
2013-06-02 18:31:49 +0200

    Translation complete.

--- include/SugarObjects/templates/company/language/application/de_DE.lang.php
+++ include/SugarObjects/templates/company/language/application/de_DE.lang.php
@@ -15,16 +15,16 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 $app_list_strings = array (
 	strtolower($object_name).'_type_dom' => array (
 		'' => '',
-		'Analyst' => 'Analyst',
-		'Competitor' => 'Competitor',
-		'Customer' => 'Customer',
+		'Analyst'    => 'Analyst',
+		'Competitor' => 'Konkurrent',
+		'Customer'   => 'Kunde',
 		'Integrator' => 'Integrator',
-		'Investor' => 'Investor',
-		'Partner' => 'Partner',
-		'Press' => 'Press',
-		'Prospect' => 'Prospect',
-		'Reseller' => 'Reseller',
-		'Other' => 'Other',
+		'Investor'   => 'Geldgeber',
+		'Partner'    => 'Partner',
+		'Press'      => 'Presse',
+		'Prospect'   => 'Aussicht',
+		'Reseller'   => 'Wiederverkäufer',
+		'Other'      => 'Andere',
 	),
 );
 
