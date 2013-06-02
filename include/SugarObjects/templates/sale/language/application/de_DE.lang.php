6d7270ff82e6f3cb22d0ce2917dbcfed0b0fb66c
tiv-source <opensource@tiv-consulting.de>
2013-06-02 20:17:02 +0200

    Add english template. Translation done.

---
+++ include/SugarObjects/templates/sale/language/application/de_DE.lang.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Language - Files for Sugar Community Edition
+ * Version 6.5.13
+ *
+ * Author: Marc Michele
+ * Company: T.I.V. Consulting GmbH
+ * URL: http://www.tiv-consulting.de/
+ *
+ */
+
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+$app_list_strings = array (
+	strtolower($object_name).'_type_dom' =>	array (
+		'' => '',
+		'Existing Business' => 'Existierendes Geschäft',
+		'New Business' => 'Neues Geschäft',
+	),
+);
+
+?>
