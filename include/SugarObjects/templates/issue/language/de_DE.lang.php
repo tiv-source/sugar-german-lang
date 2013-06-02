5adfca27f7936b40e5b4809c64391349d59921cc
tiv-source <opensource@tiv-consulting.de>
2013-06-02 20:51:31 +0200

    Translation done.

--- include/SugarObjects/templates/issue/language/de_DE.lang.php
+++ include/SugarObjects/templates/issue/language/de_DE.lang.php
@@ -7,33 +7,32 @@
  * Company: T.I.V. Consulting GmbH
  * URL: http://www.tiv-consulting.de/
  *
- * TODO: Übersetzung
  */
 
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 $mod_strings = array (
 		'LBL_NAME'             => 'Name',
-		'LBL_NUMBER'           => 'Number:',
+		'LBL_NUMBER'           => 'Nummer:',
 		'LBL_STATUS'           => 'Status:',
-		'LBL_PRIORITY'         => 'Priority:',
-		'LBL_DESCRIPTION'      => 'Description:',
-		'LBL_RESOLUTION'       => 'Resolution',
-		'LBL_LAST_MODIFIED'    => 'Last Modified',
-		'LBL_ASSIGNED_TO_ID'   => 'Assigned To:',
-		'LBL_ASSIGNED_TO_NAME' => 'User:',
-		'LBL_WORK_LOG'         => 'Work Log:',
-		'LBL_CREATED_BY'       => 'Created by:',
-		'LBL_DATE_CREATED'     => 'Date Created:',
-		'LBL_DATE_ENTERED'     => 'Date Created:',
-		'LBL_DATE_MODIFIED'    => 'Date Modified:',
-		'LBL_MODIFIED_BY'      => 'Last Modified by:',
-		'LBL_ASSIGNED_USER'    => 'Assigned User:',
+		'LBL_PRIORITY'         => 'Priorität:',
+		'LBL_DESCRIPTION'      => 'Beschreibung:',
+		'LBL_RESOLUTION'       => 'Auflösung',
+		'LBL_LAST_MODIFIED'    => 'zuletzt geändert:',
+		'LBL_ASSIGNED_TO_ID'   => 'zugewiesen an:',
+		'LBL_ASSIGNED_TO_NAME' => 'Benutzer:',
+		'LBL_WORK_LOG'         => 'Arbeits Log:',
+		'LBL_CREATED_BY'       => 'erstellt von:',
+		'LBL_DATE_CREATED'     => 'erstellt am:',
+		'LBL_DATE_ENTERED'     => 'erstellt am:',
+		'LBL_DATE_MODIFIED'    => 'zuletzt geändert:',
+		'LBL_MODIFIED_BY'      => 'zuletzt geändert von:',
+		'LBL_ASSIGNED_USER'    => 'zugewiesen an:',
 		'LBL_SYSTEM_ID'        => 'System Id:',
-		'LBL_TYPE'             => 'Type:',
-		'LBL_SUBJECT'          => 'Subject:',
-		'LBL_EDIT_BUTTON'      => 'Edit',
-		'LBL_REMOVE'           => 'Remove',
+		'LBL_TYPE'             => 'Typ:',
+		'LBL_SUBJECT'          => 'Betreff:',
+		'LBL_EDIT_BUTTON'      => 'Bearbeiten',
+		'LBL_REMOVE'           => 'Löschen',
 );
 
 ?>
