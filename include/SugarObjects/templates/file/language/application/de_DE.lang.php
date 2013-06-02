9419c807e614e723d0433faa7af9eaf35c776fb4
tiv-source <opensource@tiv-consulting.de>
2013-06-02 19:57:34 +0200

    Code cleanup.

--- include/SugarObjects/templates/file/language/application/de_DE.lang.php
+++ include/SugarObjects/templates/file/language/application/de_DE.lang.php
@@ -14,31 +14,28 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 
 $app_list_strings = array (
-		strtolower($object_name).'_category_dom' =>
-		array (
-				'' => '',
-				'Marketing' => 'Marketing',
-				'Knowledege Base' => 'Knowledge Base',
-				'Sales' => 'Sales',
-		),
+	strtolower($object_name).'_category_dom' => array (
+		'' => '',
+		'Marketing' => 'Marketing',
+		'Knowledege Base' => 'Knowledge Base',
+		'Sales' => 'Sales',
+	),
 
-		strtolower($object_name).'_subcategory_dom' =>
-		array (
-				'' => '',
-				'Marketing Collateral' => 'Marketing Collateral',
-				'Product Brochures' => 'Product Brochures',
-				'FAQ' => 'FAQ',
-		),
+	strtolower($object_name).'_subcategory_dom' => array (
+		'' => '',
+		'Marketing Collateral' => 'Marketing Collateral',
+		'Product Brochures' => 'Product Brochures',
+		'FAQ' => 'FAQ',
+	),
 
-		strtolower($object_name).'_status_dom' =>
-		array (
-				'Active' => 'Active',
-				'Draft' => 'Draft',
-				'FAQ' => 'FAQ',
-				'Expired' => 'Expired',
-				'Under Review' => 'Under Review',
-				'Pending' => 'Pending',
-		),
+	strtolower($object_name).'_status_dom' => array (
+		'Active' => 'Active',
+		'Draft' => 'Draft',
+		'FAQ' => 'FAQ',
+		'Expired' => 'Expired',
+		'Under Review' => 'Under Review',
+		'Pending' => 'Pending',
+	),
 );
 
 
