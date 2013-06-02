e1f61a69b1f543471fc998e997476daa5eb030d1
tiv-source <opensource@tiv-consulting.de>
2013-06-02 20:00:47 +0200

    Added english template.

---
+++ include/SugarObjects/templates/issue/language/application/de_DE.lang.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Language - Files for Sugar Community Edition
+ * Version 6.5.13
+ *
+ * Author: Marc Michele
+ * Company: T.I.V. Consulting GmbH
+ * URL: http://www.tiv-consulting.de/
+ *
+ * TODO: Übersetzung
+ */
+
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+$mod_strings = array (
+
+		'LBL_NAME' =>'Name',
+		'LBL_NUMBER' => 'Number:',
+		'LBL_STATUS' => 'Status:',
+		'LBL_PRIORITY' => 'Priority:',
+		'LBL_DESCRIPTION' => 'Description:',
+		'LBL_RESOLUTION' => 'Resolution',
+		'LBL_LAST_MODIFIED' => 'Last Modified',
+		'LBL_ASSIGNED_TO_ID'=>'Assigned To:',
+		'LBL_ASSIGNED_TO_NAME' => 'User:',
+		'LBL_WORK_LOG' => 'Work Log:',
+		'LBL_CREATED_BY' => 'Created by:',
+		'LBL_DATE_CREATED' => 'Date Created:',
+		'LBL_DATE_ENTERED' => 'Date Created:',
+		'LBL_DATE_MODIFIED'=>'Date Modified:',
+		'LBL_MODIFIED_BY' => 'Last Modified by:',
+		'LBL_ASSIGNED_USER' => 'Assigned User:',
+		'LBL_SYSTEM_ID' =>'System Id:',
+		'LBL_TYPE'=>'Type:',
+		'LBL_SUBJECT' => 'Subject:',
+
+		'LBL_EDIT_BUTTON' => 'Edit',
+		'LBL_REMOVE' => 'Remove',
+
+);
+
+
+?>
