f2edd112d295ca7d98d9cf1d8dcc97732e8727ff
tiv-source <opensource@tiv-consulting.de>
2013-06-02 22:24:51 +0200

    Translation done.

--- include/SugarObjects/templates/file/language/de_DE.lang.php
+++ include/SugarObjects/templates/file/language/de_DE.lang.php
@@ -7,7 +7,6 @@
  * Company: T.I.V. Consulting GmbH
  * URL: http://www.tiv-consulting.de/
  *
- * TODO: Übersetzung
  */
 
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
@@ -42,43 +41,39 @@ $mod_strings = array (
 	'LBL_FILENAME'              => 'Datei Name:',
 	'LBL_FILE_UPLOAD'           => 'Datei:',
 	'LBL_DOC_VERSION'           => 'Revision:',
-	'LBL_CATEGORY_VALUE'        => 'Category:',
-	'LBL_SUBCATEGORY_VALUE'     => 'Sub Category:',
+	'LBL_CATEGORY_VALUE'        => 'Kategorie:',
+	'LBL_SUBCATEGORY_VALUE'     => 'Unterkategorie:',
 	'LBL_DOC_STATUS'            => 'Status:',
-	'LBL_DET_TEMPLATE_TYPE'     => 'Document Type:',
-	'LBL_DOC_DESCRIPTION'       => 'Description:',
-	'LBL_DOC_ACTIVE_DATE'       => 'Publish Date:',
-	'LBL_DOC_EXP_DATE'          => 'Expiration Date:',
-	//document list view.
-	'LBL_LIST_FORM_TITLE'       => 'Document List',
-	'LBL_LIST_DOCUMENT'         => 'Document',
-	'LBL_LIST_CATEGORY'         => 'Category',
-	'LBL_LIST_SUBCATEGORY'      => 'Sub Category',
+	'LBL_DET_TEMPLATE_TYPE'     => 'Dokumenten Typ:',
+	'LBL_DOC_DESCRIPTION'       => 'Beschreibung:',
+	'LBL_DOC_ACTIVE_DATE'       => 'Veröffentlichungsdatum:',
+	'LBL_DOC_EXP_DATE'          => 'Verfallsdatum:',
+	'LBL_LIST_FORM_TITLE'       => 'Dokumenten Liste',
+	'LBL_LIST_DOCUMENT'         => 'Dokument',
+	'LBL_LIST_CATEGORY'         => 'Kategorie',
+	'LBL_LIST_SUBCATEGORY'      => 'Unterkategorie',
 	'LBL_LIST_REVISION'         => 'Revision',
-	'LBL_LIST_LAST_REV_CREATOR' => 'Published By',
-	'LBL_LIST_LAST_REV_DATE'    => 'Revision Date',
-	'LBL_LIST_VIEW_DOCUMENT'    => 'View',
-	'LBL_LIST_DOWNLOAD'         => 'Download',
-	'LBL_LIST_ACTIVE_DATE'      => 'Publish Date',
-	'LBL_LIST_EXP_DATE'         => 'Expiration Date',
+	'LBL_LIST_LAST_REV_CREATOR' => 'Veröffentlicht von',
+	'LBL_LIST_LAST_REV_DATE'    => 'Revisions Datum',
+	'LBL_LIST_VIEW_DOCUMENT'    => 'Anzeigen',
+	'LBL_LIST_DOWNLOAD'         => 'Herunterladen',
+	'LBL_LIST_ACTIVE_DATE'      => 'Veröffentlichungsdatum',
+	'LBL_LIST_EXP_DATE'         => 'Verfallsdatum',
 	'LBL_LIST_STATUS'           => 'Status',
-	//document search form.
-	'LBL_SF_DOCUMENT'           => 'Document Name:',
-	'LBL_SF_CATEGORY'           => 'Category:',
-	'LBL_SF_SUBCATEGORY'        => 'Sub Category:',
-	'LBL_SF_ACTIVE_DATE'        => 'Publish Date:',
-	'LBL_SF_EXP_DATE'           => 'Expiration Date:',
-	'DEF_CREATE_LOG'            => 'Document Created',
-	//error messages
-	'ERR_DOC_NAME'              => 'Document Name',
-	'ERR_DOC_ACTIVE_DATE'       => 'Publish Date',
-	'ERR_DOC_EXP_DATE'          => 'Expiration Date',
-	'ERR_FILENAME'              => 'File Name',
-	'LBL_TREE_TITLE'            => 'Documents',
-	//sub-panel vardefs.
-	'LBL_LIST_DOCUMENT_NAME'    => 'Document Name',
-	'LBL_EDIT_BUTTON'           => 'Edit ',
-	'LBL_REMOVE'                => 'Remove',
+	'LBL_SF_DOCUMENT'           => 'Dokumenten Name:',
+	'LBL_SF_CATEGORY'           => 'Kategorie:',
+	'LBL_SF_SUBCATEGORY'        => 'Unterkategorie:',
+	'LBL_SF_ACTIVE_DATE'        => 'Veröffentlichungsdatum:',
+	'LBL_SF_EXP_DATE'           => 'Verfallsdatum:',
+	'DEF_CREATE_LOG'            => 'Dokument erstellt',
+	'ERR_DOC_NAME'              => 'Dokumenten Name',
+	'ERR_DOC_ACTIVE_DATE'       => 'Veröffentlichungsdatum',
+	'ERR_DOC_EXP_DATE'          => 'Verfallsdatum',
+	'ERR_FILENAME'              => 'Datei Name',
+	'LBL_TREE_TITLE'            => 'Dokumente',
+	'LBL_LIST_DOCUMENT_NAME'    => 'Dokumenten Name',
+	'LBL_EDIT_BUTTON'           => 'Bearbeiten ',
+	'LBL_REMOVE'                => 'Löschen',
 );
 
 ?>
