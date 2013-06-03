084e42e4d4077f0c1b22482233e4cdf01f52f248
tiv-source <opensource@tiv-consulting.de>
2013-06-04 00:45:29 +0200

    Added english template.

---
+++ modules/Audit/language/de_DE.lang.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Language - Files for Sugar Community Edition
+ * Version 6.5.13
+ *
+ * Author: Marc Michele
+ * Company: T.I.V. Consulting GmbH
+ * URL: http://www.tiv-consulting.de/
+ *
+ * TODO: Übersetzung.
+ */
+
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+$mod_strings = array (
+		'LBL_FIELD_NAME' => 'Field',
+		'LBL_OLD_NAME' => 'Old Value',
+		'LBL_NEW_VALUE' => 'New Value',
+		'LBL_CREATED_BY' => 'Changed By',
+		'LBL_LIST_DATE' => 'Change Date',
+		'LBL_AUDITED_FIELDS' => 'Fields audited in this module: ',
+		'LBL_NO_AUDITED_FIELDS_TEXT' => 'There are no fields audited in this module',
+		'LBL_CHANGE_LOG' => 'Change Log',
+);
+
+?>
