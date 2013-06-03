1ca4a63fe4d56211b698e8a315b2a228d4fe4878
tiv-source <opensource@tiv-consulting.de>
2013-06-03 19:16:48 +0200

    Added english template.

---
+++ modules/ACLRoles/language/de_DE.lang.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Language - Files for Sugar Community Edition
+ * Version 6.5.13
+ *
+ * Author: Marc Michele
+ * Company: T.I.V. Consulting GmbH
+ * URL: http://www.tiv-consulting.de/
+ *
+ * TODO: Übersetzung.
+ */
+
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+$mod_strings = array (
+		'LBL_MODULE_NAME' => 'Roles',
+		'LBL_MODULE_TITLE' => 'Roles: Home',
+		'LBL_ROLE'=>'Role',
+		'LBL_NAME'=>'Name',
+		'LBL_DESCRIPTION'=>'Description',
+		'LIST_ROLES'=>'List Roles',
+		'LBL_USERS_SUBPANEL_TITLE'=>'Users',
+		'LIST_ROLES_BY_USER'=>'List Roles By User',
+		'LBL_LIST_FORM_TITLE' => 'Roles',
+		'LBL_ROLES_SUBPANEL_TITLE'=>'User Roles',
+		'LBL_SEARCH_FORM_TITLE'=>'Search',
+		'LBL_CREATE_ROLE'=>'Create Role',
+		'LBL_EDIT_VIEW_DIRECTIONS'=>'Double click on a cell to change value.',
+		'LBL_ACCESS_DEFAULT'=>'Not Set',
+		'LBL_ACTION_ADMIN'=>'Access Type',
+		'LBL_ALL'=>'All',
+		'LBL_DUPLICATE_OF'=>'Duplicate Of ',
+)
+
+?>
