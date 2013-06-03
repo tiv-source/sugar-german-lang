60d8dcf6b228f260425ce3cc1132803bd9b3e421
tiv-source <opensource@tiv-consulting.de>
2013-06-04 00:46:53 +0200

    Clode Cleanup.

--- modules/Audit/language/de_DE.lang.php
+++ modules/Audit/language/de_DE.lang.php
@@ -13,14 +13,14 @@
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 $mod_strings = array (
-		'LBL_FIELD_NAME' => 'Field',
-		'LBL_OLD_NAME' => 'Old Value',
-		'LBL_NEW_VALUE' => 'New Value',
-		'LBL_CREATED_BY' => 'Changed By',
-		'LBL_LIST_DATE' => 'Change Date',
-		'LBL_AUDITED_FIELDS' => 'Fields audited in this module: ',
-		'LBL_NO_AUDITED_FIELDS_TEXT' => 'There are no fields audited in this module',
-		'LBL_CHANGE_LOG' => 'Change Log',
+	'LBL_FIELD_NAME'             => 'Field',
+	'LBL_OLD_NAME'               => 'Old Value',
+	'LBL_NEW_VALUE'              => 'New Value',
+	'LBL_CREATED_BY'             => 'Changed By',
+	'LBL_LIST_DATE'              => 'Change Date',
+	'LBL_AUDITED_FIELDS'         => 'Fields audited in this module: ',
+	'LBL_NO_AUDITED_FIELDS_TEXT' => 'There are no fields audited in this module',
+	'LBL_CHANGE_LOG'             => 'Change Log',
 );
 
 ?>
