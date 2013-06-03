03242c031f450970b15859683d8a2ed4c758768e
tiv-source <opensource@tiv-consulting.de>
2013-06-03 19:16:17 +0200

    Added english template.

---
+++ modules/ACLActions/language/de_DE.lang.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Language - Files for Sugar Community Edition
+ * Version 6.5.13
+ *
+ * Author: Marc Michele
+ * Company: T.I.V. Consulting GmbH
+ * URL: http://www.tiv-consulting.de/
+ *
+ * TODO: Übersetzung.
+ */
+
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+$mod_strings = array (
+		'LBL_ACCESS_ALL'=>'All',
+		'LBL_ACCESS_NONE'=>'None',
+		'LBL_ACCESS_OWNER'=>'Owner',
+		'LBL_ACCESS_NORMAL'=>'Normal',
+		'LBL_ACCESS_ADMIN'=>'Admin',
+		'LBL_ACCESS_ENABLED'=>'Enabled',
+		'LBL_ACCESS_DISABLED'=>'Disabled',
+		'LBL_ACCESS_DEV'=>'Developer',
+		'LBL_ACCESS_ADMIN_DEV'=>'Admin & Developer',
+		'LBL_NAME'=>'Name',
+		'LBL_DESCRIPTION'=>'Description',
+		'LIST_ROLES'=>'List Roles',
+		'LBL_USERS_SUBPANEL_TITLE'=>'Users',
+		'LIST_ROLES_BY_USER'=>'List Roles By User',
+		'LBL_ROLES_SUBPANEL_TITLE'=>'User Roles',
+		'LBL_SEARCH_FORM_TITLE'=>'Search',
+		'LBL_ACTION_VIEW'=>'View',
+		'LBL_ACTION_EDIT'=>'Edit',
+		'LBL_ACTION_DELETE'=>'Delete',
+		'LBL_ACTION_IMPORT'=>'Import',
+		'LBL_ACTION_EXPORT'=>'Export',
+		'LBL_ACTION_LIST'=>'List',
+		'LBL_ACTION_MASSUPDATE' => 'Mass Update',
+		'LBL_ACTION_ACCESS'=>'Access',
+		'LBL_ACTION_ADMIN'=>'Access Type',
+		'LBL_ACCESS_DEFAULT'=>'Not Set',
+)
+
+?>
