b76d78e15e09637d5b4dd4cf226bb98950693e72
tiv-source <opensource@tiv-consulting.de>
2013-06-03 19:15:46 +0200

    Added english template.

---
+++ modules/ACL/language/de_DE.lang.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Language - Files for Sugar Community Edition
+ * Version 6.5.13
+ *
+ * Author: Marc Michele
+ * Company: T.I.V. Consulting GmbH
+ * URL: http://www.tiv-consulting.de/
+ *
+ * TODO: Übersetzung.
+ */
+
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+$mod_strings = array (
+		'LBL_ALLOW_ALL'=>'All',
+		'LBL_ALLOW_NONE'=>'None',
+		'LBL_ALLOW_OWNER'=>'Owner',
+		'LBL_ROLE'=>'Role',
+		'LBL_NAME'=>'Name',
+		'LBL_DESCRIPTION'=>'Description',
+		'LIST_ROLES'=>'List Roles',
+		'LBL_USERS_SUBPANEL_TITLE'=>'Users',
+		'LIST_ROLES_BY_USER'=>'List Roles By User',
+		'LBL_ROLES_SUBPANEL_TITLE'=>'User Roles',
+		'LBL_SEARCH_FORM_TITLE'=>'Search',
+		'LBL_NO_ACCESS'=>'You do not have access to this area. Contact your site administrator to obtain access.',
+		'LBL_REDIRECT_TO_HOME'=>'Redirect to Home in',
+		'LBL_SECONDS'=>'seconds',
+		'LBL_ADDING'=>'Adding for ',
+
+)
+
+
+?>
