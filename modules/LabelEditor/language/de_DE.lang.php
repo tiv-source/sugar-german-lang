cd463594b9c6ba001171f292b0f5f72e5f82cd22
tiv-source <opensource@tiv-consulting.de>
2013-06-04 23:00:37 +0200

    OPEN - task 2: Add english templates http://github.com/tiv-source/sugar-german-lang/issues/issue/2

---
+++ modules/LabelEditor/language/de_DE.lang.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Language - Files for Sugar Community Edition
+ * Version 6.5.13
+ *
+ * Author: Marc Michele
+ * Company: T.I.V. Consulting GmbH
+ * URL: http://www.tiv-consulting.de/
+ *
+ * TODO: Übersetzung.
+ */
+
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+$mod_strings = array(
+		'LBL_MODULE_NAME'=>'Label Editor',
+		'LBL_KEY'=>'Key:',
+		'LBL_VALUE'=>'Value:',
+);
+
+
+?>
