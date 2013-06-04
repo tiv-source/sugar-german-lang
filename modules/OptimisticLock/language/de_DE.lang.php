b26eb8a85302d0c0ff8ae151f8a801d51a08b146
tiv-source <opensource@tiv-consulting.de>
2013-06-04 23:30:37 +0200

    OPEN - task 2: Add english templates http://github.com/tiv-source/sugar-german-lang/issues/issue/2

---
+++ modules/OptimisticLock/language/de_DE.lang.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Language - Files for Sugar Community Edition
+ * Version 6.5.13
+ *
+ * Author: Marc Michele
+ * Company: T.I.V. Consulting GmbH
+ * URL: http://www.tiv-consulting.de/
+ *
+ * TODO: Übersetzung.
+ */
+
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+$mod_strings = array (
+		'LBL_YOURS' => 'Yours',
+		'LBL_IN_DATABASE' => 'In Database',
+		'LBL_CONFLICT_EXISTS' => 'A Conflict Exists For - ',
+		'LBL_ACCEPT_DATABASE' => 'Accept Database',
+		'LBL_ACCEPT_YOURS' => 'Accept Yours',
+		'LBL_RECORDS_MATCH' => 'Records Match',
+		'LBL_NO_LOCKED_OBJECTS' => 'No Locked Objects',
+
+);
+
+?>
