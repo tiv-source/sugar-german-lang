9702f59a022151b3488aed4417b9088ed6dc2ad9
tiv-source <opensource@tiv-consulting.de>
2013-06-04 21:09:22 +0200

    OPEN - task 2: Add english templates http://github.com/tiv-source/sugar-german-lang/issues/issue/2

---
+++ modules/Groups/language/de_DE.lang.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Language - Files for Sugar Community Edition
+ * Version 6.5.13
+ *
+ * Author: Marc Michele
+ * Company: T.I.V. Consulting GmbH
+ * URL: http://www.tiv-consulting.de/
+ *
+ * TODO: Übersetzung.
+ */
+
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+$mod_strings = array (
+		'LBL_MODULE_NAME'                       => 'Groups',
+		'LBL_GROUP_NAME'                        => 'Group Name:',
+		'LBL_DESCRIPTION'                       => 'Description:',
+		'LBL_TEAM'                                      => 'Team:',
+		// ListView
+		'LBL_LIST_TITLE'                        => 'Groups',
+		// Links
+		'LNK_ALL_GROUPS'                        => 'All Groups',
+		'LNK_NEW_GROUP'                         => 'Create Group',
+		'LNK_CONVERT_USER'                      => 'Convert User to Group',
+);
+
+?>
