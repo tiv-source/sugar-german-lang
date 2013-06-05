3619d62c3abdfe8765c0013b970093556f79d0ef
tiv-source <opensource@tiv-consulting.de>
2013-06-05 20:55:44 +0200

    OPEN - task 3: Cleanup Templates http://github.com/tiv-source/sugar-german-lang/issues/issue/3

--- modules/Releases/language/de_DE.lang.php
+++ modules/Releases/language/de_DE.lang.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * Language - Files for Sugar Community Edition
  * Version 6.5.13
  *
@@ -10,32 +10,31 @@
  * TODO: Übersetzung.
  */
 
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+if (!defined('sugarEntry') || !sugarEntry)
+	die('Not A Valid Entry Point');
 
-$mod_strings = array (
-		'LBL_MODULE_NAME' => 'Releases',
-		'LBL_MODULE_TITLE' => 'Releases: Home',
-		'LBL_SEARCH_FORM_TITLE' => 'Release Search',
-		'LBL_LIST_FORM_TITLE' => 'Release List',
-		'LBL_NEW_FORM_TITLE' => 'New Release',
-		'LBL_RELEASE' => 'Release:',
-		'LBL_LIST_NAME' => 'Release',
-		'LBL_NAME' => 'Release version:',
-		'LBL_LIST_LIST_ORDER' => 'Order',
-		'LBL_LIST_ORDER' => 'Order:',
-		'LBL_LIST_STATUS' => 'Status',
-		'LBL_STATUS' => 'Status:',
-		'LNK_NEW_RELEASE' => 'Release List',
-		'NTC_DELETE_CONFIRMATION' => 'Are you sure you want to delete this record?',
-		'ERR_DELETE_RECORD' => 'You must specify a record number to delete the release.',
-		'NTC_STATUS' => 'Set status to Inactive to remove this release from the Release dropdown lists',
-		'NTC_LIST_ORDER' => 'Set the order this release will appear in the Release dropdown lists',
-		'release_status_dom' =>
-		array (
-				'Active' => 'Active',
-				'Inactive' => 'Inactive',
-		),
-		'LBL_EDITLAYOUT' => 'Edit Layout' /*for 508 compliance fix*/,
+$mod_strings = array(
+	'LBL_MODULE_NAME'         => 'Releases',
+	'LBL_MODULE_TITLE'        => 'Releases: Home',
+	'LBL_SEARCH_FORM_TITLE'   => 'Release Search',
+	'LBL_LIST_FORM_TITLE'     => 'Release List',
+	'LBL_NEW_FORM_TITLE'      => 'New Release',
+	'LBL_RELEASE'             => 'Release:',
+	'LBL_LIST_NAME'           => 'Release',
+	'LBL_NAME'                => 'Release version:',
+	'LBL_LIST_LIST_ORDER'     => 'Order',
+	'LBL_LIST_ORDER'          => 'Order:',
+	'LBL_LIST_STATUS'         => 'Status',
+	'LBL_STATUS'              => 'Status:',
+	'LNK_NEW_RELEASE'         => 'Release List',
+	'NTC_DELETE_CONFIRMATION' => 'Are you sure you want to delete this record?',
+	'ERR_DELETE_RECORD'       => 'You must specify a record number to delete the release.',
+	'NTC_STATUS'              => 'Set status to Inactive to remove this release from the Release dropdown lists',
+	'NTC_LIST_ORDER'          => 'Set the order this release will appear in the Release dropdown lists',
+	'release_status_dom'      => array(
+		'Active'   => 'Active',
+		'Inactive' => 'Inactive',),
+	'LBL_EDITLAYOUT'          => 'Edit Layout' /*for 508 compliance fix*/,
 );
 
 ?>
