590fce9a80334c74d32a389f56507cdafde49f1f
tiv-source <opensource@tiv-consulting.de>
2013-06-05 18:41:20 +0200

    OPEN - task 3: Cleanup Templates http://github.com/tiv-source/sugar-german-lang/issues/issue/3

--- modules/LabelEditor/language/de_DE.lang.php
+++ modules/LabelEditor/language/de_DE.lang.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * Language - Files for Sugar Community Edition
  * Version 6.5.13
  *
@@ -10,13 +10,13 @@
  * TODO: Übersetzung.
  */
 
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+if (!defined('sugarEntry') || !sugarEntry)
+	die('Not A Valid Entry Point');
 
 $mod_strings = array(
-		'LBL_MODULE_NAME'=>'Label Editor',
-		'LBL_KEY'=>'Key:',
-		'LBL_VALUE'=>'Value:',
+	'LBL_MODULE_NAME' => 'Label Editor',
+	'LBL_KEY'         => 'Key:',
+	'LBL_VALUE'       => 'Value:',
 );
-
 
 ?>
