6ab8083116939fbd6896fbd41206451cfadc6c42
tiv-source <opensource@tiv-consulting.de>
2013-06-05 18:09:35 +0200

    OPEN - task 3: Cleanup Templates http://github.com/tiv-source/sugar-german-lang/issues/issue/3

--- modules/Groups/language/de_DE.lang.php
+++ modules/Groups/language/de_DE.lang.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * Language - Files for Sugar Community Edition
  * Version 6.5.13
  *
@@ -10,19 +10,19 @@
  * TODO: Übersetzung.
  */
 
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$mod_strings = array (
-		'LBL_MODULE_NAME'                       => 'Groups',
-		'LBL_GROUP_NAME'                        => 'Group Name:',
-		'LBL_DESCRIPTION'                       => 'Description:',
-		'LBL_TEAM'                                      => 'Team:',
-		// ListView
-		'LBL_LIST_TITLE'                        => 'Groups',
-		// Links
-		'LNK_ALL_GROUPS'                        => 'All Groups',
-		'LNK_NEW_GROUP'                         => 'Create Group',
-		'LNK_CONVERT_USER'                      => 'Convert User to Group',
+$mod_strings = array(
+	'LBL_MODULE_NAME'  => 'Groups',
+	'LBL_GROUP_NAME'   => 'Group Name:',
+	'LBL_DESCRIPTION'  => 'Description:',
+	'LBL_TEAM'         => 'Team:',
+	// ListView
+	'LBL_LIST_TITLE'   => 'Groups',
+	// Links
+	'LNK_ALL_GROUPS'   => 'All Groups',
+	'LNK_NEW_GROUP'    => 'Create Group',
+	'LNK_CONVERT_USER' => 'Convert User to Group',
 );
 
 ?>
