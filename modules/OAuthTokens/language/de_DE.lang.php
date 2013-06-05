27fc3513c84322be301e914b4f82e02550a1ac4d
tiv-source <opensource@tiv-consulting.de>
2013-06-05 19:52:11 +0200

    OPEN - task 3: Cleanup Templates http://github.com/tiv-source/sugar-german-lang/issues/issue/3

--- modules/OAuthTokens/language/de_DE.lang.php
+++ modules/OAuthTokens/language/de_DE.lang.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * Language - Files for Sugar Community Edition
  * Version 6.5.13
  *
@@ -10,21 +10,21 @@
  * TODO: Übersetzung.
  */
 
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$mod_strings = array (
-		'LBL_OAUTH_REQUEST' => "Request Token",
-		'LBL_OAUTH_AUTHORIZE' => "Authorize Token",
-		'LBL_OAUTH_CONSUMERREQ' => "Authorize token from consumer <b>%s</b>?",
-		'LBL_OAUTH_ROLE' => "Token role",
-		'LBL_OAUTH_VALIDATION' => "Verification code",
-		'LBL_ASSIGNED_TO_NAME' => 'User',
-		'LBL_ID' => 'ID',
-		'LBL_STATUS' => 'Status',
-		'LBL_TS' => 'Timestamp',
-		'LBL_LIST_DELETE' => 'Delete Token',
-		'LBL_CONSUMER' => 'Consumer Name',
-		'LBL_OAUTH_DISABLED' => 'OAuth support not enabled. PHP oauth extension may be missing. Please contact your administrator.',
+$mod_strings = array(
+	'LBL_OAUTH_REQUEST'     => "Request Token",
+	'LBL_OAUTH_AUTHORIZE'   => "Authorize Token",
+	'LBL_OAUTH_CONSUMERREQ' => "Authorize token from consumer <b>%s</b>?",
+	'LBL_OAUTH_ROLE'        => "Token role",
+	'LBL_OAUTH_VALIDATION'  => "Verification code",
+	'LBL_ASSIGNED_TO_NAME'  => 'User',
+	'LBL_ID'                => 'ID',
+	'LBL_STATUS'            => 'Status',
+	'LBL_TS'                => 'Timestamp',
+	'LBL_LIST_DELETE'       => 'Delete Token',
+	'LBL_CONSUMER'          => 'Consumer Name',
+	'LBL_OAUTH_DISABLED'    => 'OAuth support not enabled. PHP oauth extension may be missing. Please contact your administrator.',
 );
 
 ?>
