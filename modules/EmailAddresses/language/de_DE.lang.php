446782f9e22f2997d30e9c7ff503071142ea0aef
tiv-source <opensource@tiv-consulting.de>
2013-06-05 02:47:47 +0200

    OPEN - task 3: Cleanup Templates http://github.com/tiv-source/sugar-german-lang/issues/issue/3

--- modules/EmailAddresses/language/de_DE.lang.php
+++ modules/EmailAddresses/language/de_DE.lang.php
@@ -13,15 +13,14 @@
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 $mod_strings = array (
-		'LBL_EMAIL_ADDRESS_ID' => 'ID',
-		'LBL_EMAIL_ADDRESS' => 'Email Address',
-		'LBL_EMAIL_ADDRESS_CAPS' => 'Email Address caps',
-		'LBL_INVALID_EMAIL' => 'Invalid Email',
-		'LBL_OPT_OUT' => 'Opted Out',
-		'LBL_DATE_CREATE' => 'Date Create',
-		'LBL_DATE_MODIFIED' => 'Date Modified',
-		'LBL_DELETED' => 'Delete',
+	'LBL_EMAIL_ADDRESS_ID'   => 'ID',
+	'LBL_EMAIL_ADDRESS'      => 'Email Address',
+	'LBL_EMAIL_ADDRESS_CAPS' => 'Email Address caps',
+	'LBL_INVALID_EMAIL'      => 'Invalid Email',
+	'LBL_OPT_OUT'            => 'Opted Out',
+	'LBL_DATE_CREATE'        => 'Date Create',
+	'LBL_DATE_MODIFIED'      => 'Date Modified',
+	'LBL_DELETED'            => 'Delete',
 );
-
 
 ?>
