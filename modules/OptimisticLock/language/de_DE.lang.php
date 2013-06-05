19dc440ac05fc34c48bfd9bd5b7af292c3c4674f
tiv-source <opensource@tiv-consulting.de>
2013-06-05 20:31:27 +0200

    OPEN - task 3: Cleanup Templates http://github.com/tiv-source/sugar-german-lang/issues/issue/3

--- modules/OptimisticLock/language/de_DE.lang.php
+++ modules/OptimisticLock/language/de_DE.lang.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * Language - Files for Sugar Community Edition
  * Version 6.5.13
  *
@@ -10,17 +10,16 @@
  * TODO: Übersetzung.
  */
 
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$mod_strings = array (
-		'LBL_YOURS' => 'Yours',
-		'LBL_IN_DATABASE' => 'In Database',
-		'LBL_CONFLICT_EXISTS' => 'A Conflict Exists For - ',
-		'LBL_ACCEPT_DATABASE' => 'Accept Database',
-		'LBL_ACCEPT_YOURS' => 'Accept Yours',
-		'LBL_RECORDS_MATCH' => 'Records Match',
-		'LBL_NO_LOCKED_OBJECTS' => 'No Locked Objects',
-
+$mod_strings = array(
+	'LBL_YOURS'             => 'Yours',
+	'LBL_IN_DATABASE'       => 'In Database',
+	'LBL_CONFLICT_EXISTS'   => 'A Conflict Exists For - ',
+	'LBL_ACCEPT_DATABASE'   => 'Accept Database',
+	'LBL_ACCEPT_YOURS'      => 'Accept Yours',
+	'LBL_RECORDS_MATCH'     => 'Records Match',
+	'LBL_NO_LOCKED_OBJECTS' => 'No Locked Objects',
 );
 
 ?>
